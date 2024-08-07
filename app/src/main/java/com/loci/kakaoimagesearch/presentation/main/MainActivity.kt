d76c3e3f784320f469627fac85c14dfa9ad0d4ba
kdbswo <kdbswo@gmail.com>
2024-08-07 15:53:52 +0900

    Add get kakao hash-code

--- app/src/main/java/com/loci/kakaoimagesearch/presentation/main/MainActivity.kt
+++ app/src/main/java/com/loci/kakaoimagesearch/presentation/main/MainActivity.kt
@@ -1,20 +1,44 @@
 package com.loci.kakaoimagesearch.presentation.main
 
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
+import android.util.Base64
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import com.loci.kakaoimagesearch.R
 import com.loci.kakaoimagesearch.databinding.ActivityMainBinding
+import java.security.MessageDigest
 
 class MainActivity : AppCompatActivity() {
 
     private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
 
+    @RequiresApi(Build.VERSION_CODES.P)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(binding.root)
+
+        try {
+            val information = packageManager.getPackageInfo(packageName, PackageManager.GET_SIGNING_CERTIFICATES)
+            val signatures = information.signingInfo.apkContentsSigners
+            for (signature in signatures) {
+                val md = MessageDigest.getInstance("SHA").apply {
+                    update(signature.toByteArray())
+                }
+                val HASH_CODE = String(Base64.encode(md.digest(), 0))
+
+                Log.d("TAG", "HASH_CODE -> $HASH_CODE")
+            }
+        } catch (e: Exception) {
+            Log.d("TAG", "Exception -> $e")
+        }
+
+
 
         initNavigationBar()
         binding.bottomNavigationBar.selectedItemId = R.id.homeItem
