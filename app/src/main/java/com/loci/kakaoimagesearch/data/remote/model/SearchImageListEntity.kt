46309aaeb6e788b62d9fcf63f5820ce28921509c
kdbswo <kdbswo@gmail.com>
2024-08-09 11:01:08 +0900

    Add search UUID

--- app/src/main/java/com/loci/kakaoimagesearch/data/remote/model/SearchImageListEntity.kt
+++ app/src/main/java/com/loci/kakaoimagesearch/data/remote/model/SearchImageListEntity.kt
@@ -5,6 +5,8 @@ import java.util.Date
 data class SearchImageListEntity(val items: List<SearchImageEntity>)
 
 data class SearchImageEntity(
+    val uuid: String,
+    val searchType: String,
     val thumbnailUrl: String,
     val displaySitName: String,
     val datetime: Date,
--- app/src/main/java/com/loci/kakaoimagesearch/presentation/mapper/SearchImageListMapper.kt
+++ app/src/main/java/com/loci/kakaoimagesearch/presentation/mapper/SearchImageListMapper.kt
@@ -1,10 +1,13 @@
 package com.loci.kakaoimagesearch.presentation.mapper
 
 import com.google.gson.Gson
+import com.loci.kakaoimagesearch.data.remote.model.ClipDocument
 import com.loci.kakaoimagesearch.data.remote.model.KakaoSearchImageListResponse
+import com.loci.kakaoimagesearch.data.remote.model.SearchClipEntity
 import com.loci.kakaoimagesearch.data.remote.model.SearchDocument
 import com.loci.kakaoimagesearch.data.remote.model.SearchImageEntity
 import com.loci.kakaoimagesearch.data.remote.model.SearchImageListEntity
+import java.util.UUID
 
 fun KakaoSearchImageListResponse.toEntity() = documents?.let {
     SearchImageListEntity(
@@ -15,8 +18,23 @@ fun KakaoSearchImageListResponse.toEntity() = documents?.let {
 fun List<SearchDocument>.asSearchImageEntity(): List<SearchImageEntity> {
     return map {
         SearchImageEntity(
+            UUID.randomUUID().toString(),
+            "Image",
             it.thumbnailUrl ?: "",
             it.displaySiteName ?: "",
+            it.datetime,
+            false
+        )
+    }
+}
+
+fun List<ClipDocument>.asSearchClipEntity(): List<SearchClipEntity> {
+    return map {
+        SearchClipEntity(
+            UUID.randomUUID().toString(),
+            "Video",
+            it.title ?: "",
+            it.thumbnailUrl ?: "",
             it.datetime,
             false
         )
